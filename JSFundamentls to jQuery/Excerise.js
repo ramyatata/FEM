8180dc176c6e991060f5e41e6142c00b2b6b5603
ramyatata <Ramyatata007@gmail.com>
2017-04-06 19:56:42 -0500

    Update Excerise.js

--- JSFundamentls to jQuery/Excerise.js
+++ JSFundamentls to jQuery/Excerise.js
@@ -1,4 +1,4 @@
-//DOT operator
+//1.DOT operator
 
 var DOT = function(object, attr){
   if(object.hasOwnProperty(attr)){
@@ -9,3 +9,32 @@ var DOT = function(object, attr){
         DOT(object.__proto__, attr);
     }
 }
+//2.DOTCALL operator
+var DOTCAL = function(obj, method, args){
+  var fn = DOT(obj, method)
+  if(fn){
+    return fn.apply(obj,args);
+     }
+  else{
+    return undefined;
+  }
+}
+
+
+//4. NEW Operator
+var NEW = function(fn, args){
+  var obj = {};
+  obj.__proto__ = fn.prototype;
+  fn.apply(obj, args);
+  return obj;
+}
+
+var Person = function(name){
+  this.name = name;
+};
+Person.prototype.speak = function(){
+  console.log(this.name+" is speaking");
+};
+var person = NEW(Person, ['smith']);
+console.log(person.name);
+person.speak();
