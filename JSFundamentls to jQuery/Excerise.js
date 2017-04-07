2514fe3b29eb2ab97aeb99d47f2c647c61731a11
ramyatata <Ramyatata007@gmail.com>
2017-04-06 22:23:07 -0500

    Update Excerise.js

--- JSFundamentls to jQuery/Excerise.js
+++ JSFundamentls to jQuery/Excerise.js
@@ -38,3 +38,93 @@ Person.prototype.speak = function(){
 var person = NEW(Person, ['smith']);
 console.log(person.name);
 person.speak();
+
+
+//INSTANCEOF operator
+var INSTANCEOF = function(obj, fun){
+  if(obj.__proto__ === fun.prototype){
+    return true;
+  }
+  else if(obj.__proto__){
+    return INSTANCEOF(obj.__proto__, fun);
+  }
+  else{
+    return false;
+  }
+};
+
+var Person = function(name){
+  this.name = name;
+};
+var person = new Person("smith");
+console.log(INSTANCEOF(person, Person))
+
+//$.extend 
+var extend = function(target, object){
+  //loop through object properites
+  //add them to target
+  for(var key in objects){
+    if(object.hasOwnProperty(key)){
+      target[key] = object[key];
+    }
+  }
+  return target;
+};
+
+var target = {name: "smith"};
+var object = {year: "1988"};
+var res = extend(target,object);
+
+//isArray
+var isArray = function(obj){
+  if(Object.prototype.toString.call(obj) === "[object Array]"){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+//isArrayLike
+var isArrayLike = function(obj){
+  //has length property
+  //has a property length-1
+if(typeof obj.length === "number"){
+  if(obj.length === 0){
+    return true;
+  }
+  else if(object.length >0){
+    if(obj.length-1 in obj){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
+  else{
+    return true
+  }
+}
+
+//$.each
+var each = function(collection, fun){
+  if(isArrayLike(collection)){
+    for(var i=0; i < collection.length; i++){
+      var val = collection[i];
+      fun.call(i, collection[i]);
+    }
+  }
+  else{
+    for(var keys in Collection){
+      if(collection.hasOwnProperty(keys)){
+        var val = collection[keys];
+      fun.call(keys, collection[keys])
+      }
+    }
+  }
+  return collection
+}
+
+
+
